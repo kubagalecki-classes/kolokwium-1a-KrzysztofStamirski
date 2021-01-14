259c7f4f2bf1d338016a2f877f2b16d9b5e4a640
StamirskiK <01123929@pw.edu.pl>
2021-01-14 11:31:22 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,4 +1,6 @@
 #include "catch.hpp"
+#include "iostream"
+#include "vector"
 
 // Zad2
 // tutaj definicja klasy Makaron
@@ -6,3 +8,21 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle{
+  double L;
+  double W;
+  double R;
+  static const double C;
+  public:
+    Tagliatelle(double l,double w,double r): L(l), W(w), R(r){}
+
+    Tagliatelle(){
+    L=0.5;
+    W=0.5;
+    R=0.5;
+    }
+    double ileMaki(const unsigned int P){
+      return P * L * W * (1.0 - R) * C;
+    }
+
+};
