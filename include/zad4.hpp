10fdebc2eb4799e54dc6aa77feba1074fb359174
StamirskiK <01123929@pw.edu.pl>
2021-01-14 12:25:39 +0000

    zad4v1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,16 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+double obliczMake(std::vector<Tagliatelle> t){
+  Tagliatelle pierwszy = t.front()
+  Tagliatelle przedostatni = t.at(t.size()-2);
+  Tagliatelle trzeciOdKonca = t.at(t.size() - 3);
+  double M = pierwszy.ileMaki(1) + przedostatni.ileMaki(2) + trzeciOdKonca.ileMaki(3);
+  if ( M > 100 ) {
+    throw 100;
+  } else if ( M > 50 && M <= 100 ){
+    throw 55.5;
+  } else {
+    return M;
+  }
+}
